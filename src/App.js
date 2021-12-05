c7e6f4ac18c188d375910f78bfc5bf1d4f160c62
strawberrySword <shaharglame@gmail.com>
2021-12-05 20:00:31 +0200

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+// import FileBase64 from 'react-file-base64';
 
 function App() {
+  const [userDetails, setUserDetails] = useState({name:"", lastName:"", phone:"", email:"", rentEnd: ""})
+
+  const onSubmit = (e) =>{
+    e.preventDefualt()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form className="form" onSubmit={onSubmit}>
+        <h1 className="title">Hoodie.</h1>
+        <br />
+          <p>
+            הודי היא קהילה סגורה וחינמית של דירות
+            המאפשרת לראות את כל הדירות בכל רגע נתון
+            על ידי חיבור דיירים נכנסים לדיירים יוצאים
+          </p> ‍
+          <p>
+            אנחנו מאפשרים כעת הרשמה מוקדמת
+            ועובדים קשה על הגדלת הקהילה שלנו
+          </p>
+          ‍<p>
+            !מלאו את הטופס כדי לקבל גישה מוקדמת לקהילה
+          </p>
+          <br /><br />
+        <div className="form_items_holder">
+          <div className="form_item">
+            <label>שם פרטי:</label>
+            <input type="text" value={userDetails.name} onChange={(e) => {setUserDetails({...userDetails, name: e.target.value})}} required/>
+          </div>
+          <div className="form_item">
+            <label>שם משפחה:</label>
+            <input type="text" value={userDetails.lastName} onChange={(e) => {setUserDetails({...userDetails, lastName: e.target.value})}} required/>
+          </div>
+        </div>
+        <div className="form_items_holder">
+          <div className="form_item">
+            <label>טלפון:</label>
+            <input type="text" value={userDetails.phone} onChange={(e) => {setUserDetails({...userDetails, phone: e.target.value})}} required/>
+          </div>
+          <div className="form_item">
+            <label>אימייל:</label>
+            <input type="text" value={userDetails.email} onChange={(e) => {setUserDetails({...userDetails, email: e.target.value})}} required/>
+          </div>
+        </div>
+          <div className="form_item">
+            <label>חוזה שכירות מסתיים:</label>
+            <input type="email" pattern=".+@.+\.com" value={userDetails.rentEnd} onChange={(e) => {setUserDetails({...userDetails, rentEnd: e.target.value})}} required/>
+          </div>
+
+        {/* <FileBase64 
+          type="file"
+          multiple={false}
+          onDone={({base64}) => setPost({...post, image: base64})}
+        /> */}
+      </form>
     </div>
   );
 }
